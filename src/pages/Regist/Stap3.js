37d7d3deb7add625c17684def23723a0a64a40f0
Rudi Widodo <rudiwidodo271@gmail.com>
2021-04-10 23:24:06 +0700

    perbaikan halaman step 3 (29)

--- src/pages/Regist/Stap3.js
+++ src/pages/Regist/Stap3.js
@@ -132,7 +132,7 @@ export function GridRow1({ active, click }) {
                     return (
                         <div
                             key={value.id}
-                            className={`lg:col-start-${value.gridStart} xs:col-start-${value.gridStart - 2} box-grid`}>
+                            className={`box-grid`}>
                             <div
                                 onClick={() => click(value.gridStart, value.title, value.text[0].idr)}
                                 className={`xs:w-20 xs:h-20 xs:text-sm xl:w-32 xl:h-32 xl:text-2xl mx-auto bg-red-400 flex justify-center items-center text-white font-bold cursor-pointer active-box ${value.gridStart === active ? `active` : ''}`}
