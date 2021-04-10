4479eedd244a3fdb8a2202fe7a6843c209c34ae5
Rudi Widodo <rudiwidodo271@gmail.com>
2021-04-10 22:24:59 +0700

    perbaikan halaman step 3 (25)

--- src/pages/Regist/Stap3.js
+++ src/pages/Regist/Stap3.js
@@ -124,25 +124,20 @@ export function ListItems() {
 }
 
 export function GridRow1({ active, click }) {
-    return (
-        <Fragment>
-            {RegisterComponentJson.stap3.map(value => {
-                return (
-                    <div
-                        key={value.id}
-                        className={`lg:col-start-${value.gridStart} xs:col-start-${value.gridStart - 2} box-grid`}>
-                        <div
-                            onClick={() => click(value.gridStart, value.title, value.text[0].idr)}
-                            className={`xs:w-20 xs:h-20 xs:text-sm xl:w-32 xl:h-32 xl:text-2xl mx-auto bg-red-400 flex justify-center items-center text-white font-bold cursor-pointer active-box ${value.gridStart === active ? `active` : ''}`}
-                            value={value.title}>
-                            {value.title}
-                        </div>
-                    </div>
-                )
-            })
-            }
-        </Fragment>
-    )
+    RegisterComponentJson.stap3.map(value => {
+        return (
+            <div
+                key={value.id}
+                className={`lg:col-start-${value.gridStart} xs:col-start-${value.gridStart - 2} box-grid`}>
+                <div
+                    onClick={() => click(value.gridStart, value.title, value.text[0].idr)}
+                    className={`xs:w-20 xs:h-20 xs:text-sm xl:w-32 xl:h-32 xl:text-2xl mx-auto bg-red-400 flex justify-center items-center text-white font-bold cursor-pointer active-box ${value.gridStart === active ? `active` : ''}`}
+                    value={value.title}>
+                    {value.title}
+                </div>
+            </div>
+        )
+    })
 }
 
 export function GridRow2({ active }) {
